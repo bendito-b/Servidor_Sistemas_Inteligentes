43605ffb51151117cc34ee039369d380c2db6313
ZeNktr4k <0333202009@unjfsc.edu.pe>
2024-09-15 22:28:56 -0500

    Pruebita

---
+++ AccesoDatos_Proyecto_SistemasInteligentes/Models/Prueba.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccesoDatos_Proyecto_SistemasInteligentes.Models;
+
+[Table("prueba")]
+public partial class Prueba
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+}
