2b02252a6b7a68b8ca92f2f4693ed1e352878b1d
Anthony Caurino <130081627+ZeNktr4k@users.noreply.github.com>
2024-11-19 09:47:13 -0500

    Update PruebaController.cs

--- WebApi/Controllers/PruebaController.cs
+++ WebApi/Controllers/PruebaController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApi.Controllers
 {
     //Pruebita 
-    
+    //Cambio pruebita 
     [Route("api")]
     [ApiController]
     public class PruebaController : ControllerBase
