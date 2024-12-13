a36356bb37c925f76611b001ded7ef3c6aecd155
bendito-b <edir.betito@gmail.com>
2024-12-13 17:39:23 -0500

    añadir el prettier y eslint

--- Interfaces/interfaz-sistema-inteligente-minero/App.js
+++ Interfaces/interfaz-sistema-inteligente-minero/App.js
@@ -1,11 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableHighlight } from 'react-native';
+
+const sendData = async () => {
+  const url = "http://192.168.1.53:5226/api/Hechos/ActividadGeomecanica";
+
+  const data = {
+    presionGeologica: 0,
+    densidadDelSuelo: 0,
+    profundidadDeFisuras: 0,
+    indiceDeFracturamiento: 0,
+    profundidadDePerforacion: 0,
+  };
+
+  const headers = {
+    "Content-Type": "application/json",
+    "tip_sue": "2",
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    alert(JSON.stringify(responseData));
+  } catch (error) {
+    alert(error.message);
+  }
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <StatusBar style="light" />
+      <Text>Esqueleto de la aplicación</Text>
+      <Button title="pulsa aquí" onPress={() => sendData()} />
+      <Text>\n</Text>
+      <TouchableHighlight
+        underlayColor={'#09f'}
+        style={{
+          width: 200,
+          height: 200,
+          borderRadius: 100,
+          backgroundColor: '#fff',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onPress={() => sendData()}>
+          <Text>Pulsa aquí</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -13,7 +62,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
