1b30ec781a7bf5987399063de99d1af4c20d53d8
bendito-b <edir.betito@gmail.com>
2024-12-15 03:29:37 -0500

    versión 2.0

--- Interfaces/interfaz-sistema-inteligente-minero/App.js
+++ Interfaces/interfaz-sistema-inteligente-minero/App.js
@@ -7,100 +7,134 @@ import {
   TextInput,
   Pressable,
   ScrollView,
-  Platform,
+  Modal,
+  FlatList,
 } from "react-native";
 
 export default function App() {
   const [selectedSoil, setSelectedSoil] = useState(null);
   const [headerValue, setHeaderValue] = useState(1);
   const [inputValues, setInputValues] = useState({});
+  const [errorMessages, setErrorMessages] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
+  const [predictions, setPredictions] = useState([]);
 
   const soils = [
     {
       name: "Sedimentarios",
       inputs: [
-        "Presión Geológica",
-        "Densidad del Suelo",
-        "Profundidad de Fisuras",
-        "Índice de Fracturamiento",
-        "Profundidad de Perforación",
+        { name: "Presión Geológica", unit: "MPa", min: 50.0, max: 300.0 },
+        { name: "Densidad del Suelo", unit: "g/cm³", min: 1.5, max: 3.5 },
+        { name: "Profundidad de Fisuras", unit: "m", min: 100.0, max: 700.0 },
+        { name: "Índice de Fracturamiento", unit: null, min: 0.8, max: 2.8 },
+        {
+          name: "Profundidad de Perforación",
+          unit: "m",
+          min: 100.0,
+          max: 700.0,
+        },
       ],
       api: "/api/Hechos/ActividadGeomecanica",
     },
     {
       name: "Vertisoles",
       inputs: [
-        "Presión Geológica",
-        "Densidad del Suelo",
-        "Profundidad de Fisuras",
-        "Índice de Fracturamiento",
-        "Profundidad de Perforación",
+        { name: "Presión Geológica", unit: "MPa", min: 100.0, max: 400.0 },
+        { name: "Densidad del Suelo", unit: "g/cm³", min: 2.1, max: 3.7 },
+        { name: "Profundidad de Fisuras", unit: "m", min: 100.0, max: 700.0 },
+        { name: "Índice de Fracturamiento", unit: null, min: 1.2, max: 3.5 },
+        {
+          name: "Profundidad de Perforación",
+          unit: "m",
+          min: 150.0,
+          max: 800.0,
+        },
       ],
       api: "/api/Hechos/ActividadGeomecanica",
     },
     {
       name: "Residuales",
       inputs: [
-        "Elementos Traza",
-        "Concentración Óxidos Metálicos",
-        "pH",
-        "Conductividad Iónica",
-        "Grado Meteorización",
+        { name: "Elementos Traza", unit: "ppm", min: 5.0, max: 250.0 },
+        { name: "Óxidos Metálicos", unit: "%", min: 0.0, max: 30.0 },
+        { name: "Potencial de Hidrógeno", unit: null, min: 4.5, max: 8.0 },
+        { name: "Conductividad Iónica", unit: "mS/cm", min: 0.5, max: 4.0 },
+        { name: "Meteorización", unit: "%", min: 30.0, max: 90.0 },
       ],
       api: "/api/Hechos/ComposicionGeoquimica",
     },
     {
       name: "Oxisoles",
       inputs: [
-        "Elementos Traza",
-        "Concentración Óxidos Metálicos",
-        "pH",
-        "Conductividad Iónica",
-        "Grado Meteorización",
+        { name: "Elementos Traza", unit: "ppm", min: 0.0, max: 350.0 },
+        { name: "Óxidos Metálicos", unit: "%", min: 0.0, max: 35.0 },
+        { name: "Potencial de Hidrógeno", unit: null, min: 4.0, max: 7.5 },
+        { name: "Conductividad Iónica", unit: "mS/cm", min: 0.0, max: 5.0 },
+        { name: "Meteorización", unit: "%", min: 20.0, max: 100.0 },
       ],
       api: "/api/Hechos/ComposicionGeoquimica",
     },
     {
       name: "Lateritas",
       inputs: [
-        "Resistividad Eléctrica",
-        "Magnetismo",
-        "Saturación de Agua",
-        "Susceptibilidad Magnética",
-        "Contenido Óxidos Metálicos",
+        {
+          name: "Resistividad Eléctrica",
+          unit: "10⁻³ Ω·m",
+          min: 0.45,
+          max: 1.9,
+        },
+        { name: "Magnetismo", unit: "A/m", min: 1.0, max: 3.0 },
+        { name: "Saturación de Agua", unit: "%", min: 12.0, max: 33.0 },
+        { name: "Susceptibilidad Magnética", unit: "10⁻³", min: 1.0, max: 3.5 },
+        {
+          name: "Contenido de Óxidos Metálicos",
+          unit: "%",
+          min: 3.0,
+          max: 40.0,
+        },
       ],
       api: "/api/Hechos/ConductividadMagnetismo",
     },
     {
       name: "Glaciales",
       inputs: [
-        "Resistividad Eléctrica",
-        "Magnetismo",
-        "Saturación de Agua",
-        "Susceptibilidad Magnética",
-        "Contenido Óxidos Metálicos",
+        {
+          name: "Resistividad Eléctrica",
+          unit: "10⁻³ Ω·m",
+          min: 0.8,
+          max: 2.3,
+        },
+        { name: "Magnetismo", unit: "A/m", min: 0.7, max: 2.6 },
+        { name: "Saturación de Agua", unit: "%", min: 6.0, max: 30.0 },
+        { name: "Susceptibilidad Magnética", unit: "10⁻³", min: 0.5, max: 4.0 },
+        {
+          name: "Contenido de Óxidos Metálicos",
+          unit: "%",
+          min: 0.0,
+          max: 45.0,
+        },
       ],
       api: "/api/Hechos/ConductividadMagnetismo",
     },
     {
       name: "Andosoles",
       inputs: [
-        "pH",
-        "Temperatura",
-        "Porosidad",
-        "Agua Subterránea",
-        "Alteración Hidrotermal",
+        { name: "Potencial de Hidrógeno", unit: null, min: 4.2, max: 7.0 },
+        { name: "Temperatura", unit: "°C", min: 190.0, max: 440.0 },
+        { name: "Porosidad", unit: "%", min: 10.0, max: 30.0 },
+        { name: "Agua Subterránea", unit: "m³", min: 150.0, max: 450.0 },
+        { name: "Alteración Hidrotermal", unit: "%", min: 20.0, max: 70.0 },
       ],
       api: "/api/Hechos/PotencialHidrotermal",
     },
     {
       name: "Entisoles",
       inputs: [
-        "pH",
-        "Temperatura",
-        "Porosidad",
-        "Agua Subterránea",
-        "Alteración Hidrotermal",
+        { name: "Potencial de Hidrógeno", unit: null, min: 5.0, max: 7.7 },
+        { name: "Temperatura", unit: "°C", min: 230.0, max: 450.0 },
+        { name: "Porosidad", unit: "%", min: 7.0, max: 25.0 },
+        { name: "Agua Subterránea", unit: "m³", min: 100.0, max: 400.0 },
+        { name: "Alteración Hidrotermal", unit: "%", min: 15.0, max: 65.0 },
       ],
       api: "/api/Hechos/PotencialHidrotermal",
     },
@@ -110,10 +144,25 @@ export default function App() {
     setSelectedSoil(null);
     setHeaderValue(1);
     setInputValues({});
+    setErrorMessages({});
   };
 
-  const handleInputChange = (field, value) => {
-    const sanitizedField = field.replace(/\s+/g, ""); // Remove spaces from field names
+  const handleInputChange = (field, value, min, max) => {
+    const sanitizedField = field.replace(/\s+/g, "");
+    const numericValue = parseFloat(value);
+
+    if (isNaN(numericValue) || numericValue < min || numericValue > max) {
+      setErrorMessages((prev) => ({
+        ...prev,
+        [sanitizedField]: `El valor debe estar entre ${min} y ${max}.`,
+      }));
+    } else {
+      setErrorMessages((prev) => ({
+        ...prev,
+        [sanitizedField]: null,
+      }));
+    }
+
     setInputValues((prevValues) => ({
       ...prevValues,
       [sanitizedField]: value,
@@ -122,6 +171,12 @@ export default function App() {
 
   const sendData = async () => {
     if (!selectedSoil) return;
+
+    const hasErrors = Object.values(errorMessages).some((msg) => msg);
+    if (hasErrors) {
+      alert("Corrige los errores antes de continuar.");
+      return;
+    }
 
     const url = `http://192.168.1.53:5226${selectedSoil.api}`;
     const headers = {
@@ -141,7 +196,8 @@ export default function App() {
       }
 
       const responseData = await response.json();
-      alert(`Respuesta recibida: ${JSON.stringify(responseData)}`);
+      setPredictions(responseData);
+      setModalVisible(true);
     } catch (error) {
       alert(`Error: ${error.message}`);
     }
@@ -149,18 +205,33 @@ export default function App() {
 
   const renderInputs = (inputs) => (
     <View>
-      {inputs.map((label, index) => (
-        <View key={index} style={styles.inputGroup}>
-          <Text style={styles.label}>{label}</Text>
-          <TextInput
-            style={styles.input}
-            keyboardType="numeric"
-            placeholder="Ingrese valor"
-            value={inputValues[label.replace(/\s+/g, "")] || ""} // Match sanitized field names
-            onChangeText={(value) => handleInputChange(label, value)}
-          />
-        </View>
-      ))}
+      {inputs.map((input, index) => {
+        const sanitizedField = input.name.replace(/\s+/g, "");
+        return (
+          <View key={index} style={styles.inputGroup}>
+            <Text style={styles.label}>{input.name}</Text>
+            <View style={styles.inputWithUnit}>
+              <TextInput
+                style={styles.input}
+                keyboardType="numeric"
+                placeholder={`Entre ${input.min} y ${input.max}`}
+                value={inputValues[sanitizedField] || ""}
+                onChangeText={(value) =>
+                  handleInputChange(input.name, value, input.min, input.max)
+                }
+              />
+              {input.unit && <Text style={styles.unit}>{input.unit}</Text>}
+            </View>
+            <View style={styles.errorContainer}>
+              {errorMessages[sanitizedField] && (
+                <Text style={styles.errorText}>
+                  {errorMessages[sanitizedField]}
+                </Text>
+              )}
+            </View>
+          </View>
+        );
+      })}
       <Pressable style={styles.predictButton} onPress={sendData}>
         <Text style={styles.predictButtonText}>Predecir</Text>
       </Pressable>
@@ -177,7 +248,7 @@ export default function App() {
               style={styles.soilButton}
               onPress={() => {
                 setSelectedSoil(soil);
-                setHeaderValue(index % 2 === 0 ? 1 : 2); // Alternate headers by soil index
+                setHeaderValue(index % 2 === 0 ? 1 : 2);
               }}
             >
               <Text style={styles.soilButtonText}>{soil.name}</Text>
@@ -186,12 +257,44 @@ export default function App() {
         </View>
       ) : (
         <View style={styles.inputContainer}>
+          <Text style={styles.selectedSoilTitle}>{selectedSoil.name}</Text>
           <Pressable style={styles.backButton} onPress={reset}>
             <Text style={styles.backButtonText}>Regresar</Text>
           </Pressable>
           {renderInputs(selectedSoil.inputs)}
         </View>
       )}
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Predicciones</Text>
+            <FlatList
+              data={predictions}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({ item }) => (
+                <View style={styles.predictionItem}>
+                  <Text style={styles.predictionText}>{item.mineral}</Text>
+                  <Text style={styles.predictionText}>
+                    Probabilidad: {item.probabilidad}%
+                  </Text>
+                </View>
+              )}
+            />
+            <Pressable
+              style={styles.closeButton}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.closeButtonText}>Cerrar</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </ScrollView>
   );
 }
@@ -203,12 +306,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#000",
     alignItems: "center",
     paddingTop: Constants.statusBarHeight,
-    paddings: 12,
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
   },
   buttonContainer: {
     alignItems: "center",
@@ -232,6 +329,13 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: "100%",
   },
+  selectedSoilTitle: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+  },
   backButton: {
     backgroundColor: "#f44336",
     padding: 10,
@@ -252,14 +356,31 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginBottom: 5,
   },
+  inputWithUnit: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
   input: {
-    placeholderTextColor: "#888888",
+    flex: 1,
     color: "#f0f0f0",
     borderWidth: 2,
     borderColor: "#2196F3",
     borderRadius: 9,
     padding: 8,
     fontSize: 14,
+  },
+  unit: {
+    color: "#f0f0f0",
+    marginLeft: 10,
+    fontSize: 14,
+  },
+  errorContainer: {
+    minHeight: 18,
+  },
+  errorText: {
+    color: "#f44336",
+    fontSize: 12,
+    marginTop: 5,
   },
   predictButton: {
     backgroundColor: "#2196F3",
@@ -269,6 +390,43 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   predictButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    width: "80%",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 15,
+  },
+  predictionItem: {
+    marginBottom: 10,
+  },
+  predictionText: {
+    fontSize: 16,
+  },
+  closeButton: {
+    backgroundColor: "#2196F3",
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 5,
+    width: "100%",
+    alignItems: "center",
+  },
+  closeButtonText: {
     color: "#fff",
     fontSize: 16,
     fontWeight: "bold",
